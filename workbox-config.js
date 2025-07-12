efc3ccb9c5c0bdfb79f627794fcab9a57c4f47f1
ed <edcalderon>
2025-07-12 04:36:49 -0500

    v1.1.0

--- workbox-config.js
+++ workbox-config.js
@@ -4,6 +4,7 @@ module.exports = {
 		'**/*.{css,js,svg,ico,json,html}'
 	],
 	swDest: 'dist/sw.js',
+	maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // 5MB
 	ignoreURLParametersMatching: [
 		/^utm_/,
 		/^fbclid$/

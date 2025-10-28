9c01dcc202f5157ba59f2b718aa5ddc6b4988acd
ed <edwardca12@hotmail.com>
2025-10-28 01:21:41 -0500

    Add Amplify Lambda dispatcher for Expo API and copy server handlers in build
    - New function: amplify/backend/function/bslApi/src/index.mjs
    - amplify.yml: copy dist/server/_expo/functions/api into function src
    - Prepare for Amplify Gen 2 route /api/* -> bslApi

--- app/events/bsl2025/speakers/index.tsx
+++ app/events/bsl2025/speakers/index.tsx
@@ -3,3 +3,4 @@ import { Redirect } from 'expo-router';
 export default function SpeakersIndex() {
   return <Redirect href="/events/bsl2025/speakers/calendar" />;
 }
+
--- components/explorer/ExplorerHeader.tsx
+++ components/explorer/ExplorerHeader.tsx
@@ -83,3 +83,4 @@ const getStyles = (colors: any) => StyleSheet.create({
     marginTop: 2,
   },
 });
+

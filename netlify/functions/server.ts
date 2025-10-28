253a2e1c6d57d2f776440431ac09426b5f185bd1
ed <edwardca12@hotmail.com>
2025-10-28 02:33:35 -0500

    Add CORS handling to Netlify server function (OPTIONS + Access-Control headers)

--- netlify/functions/server.ts
+++ netlify/functions/server.ts
@@ -1,7 +1,39 @@
 import { createRequestHandler } from '@expo/server/adapter/netlify';
 
-const handler = createRequestHandler({
+const inner = createRequestHandler({
   build: require('path').join(__dirname, '../../dist/server'),
 });
+
+const ALLOW_ORIGIN = process.env.CORS_ALLOW_ORIGIN || 'https://bsl2025.hashpass.tech';
+const ALLOW_METHODS = 'GET,POST,OPTIONS,PATCH,DELETE';
+const ALLOW_HEADERS = 'Content-Type, Authorization';
+
+const withCors = (fn: any) => async (event: any, context: any) => {
+  // Handle preflight
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 204,
+      headers: {
+        'Access-Control-Allow-Origin': ALLOW_ORIGIN,
+        'Access-Control-Allow-Methods': ALLOW_METHODS,
+        'Access-Control-Allow-Headers': ALLOW_HEADERS,
+      },
+      body: '',
+    };
+  }
+
+  const res = await fn(event, context);
+  return {
+    ...res,
+    headers: {
+      ...(res?.headers || {}),
+      'Access-Control-Allow-Origin': ALLOW_ORIGIN,
+      'Access-Control-Allow-Methods': ALLOW_METHODS,
+      'Access-Control-Allow-Headers': ALLOW_HEADERS,
+    },
+  };
+};
+
+const handler = withCors(inner);
 
 module.exports = { handler };
